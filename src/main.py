7e70a7743d292a075ebc32bb950f2db4a4019ae3
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2022-10-26 21:10:36 -0400

    Webcam Enabled
    - theres a bunch of files added too

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
+import gradio as gr
+import numpy as np
 
-def main():
-    print('Sup')
+def flip(im):
+    #This is the output
+    return None
 
-if __name__ == '__main__':
-    main()
+with gr.Blocks(css="#warning {color: red}") as demo:
+        box1 = gr.Textbox(value="Good Job")
+        box2 = gr.Textbox(value="Failure", elem_id="warning")
+        gr.Interface(
+            flip, 
+            gr.Image(source="webcam", streaming=True), 
+            "image",
+            live=True
+        )
+ 
+if __name__ == "__main__":
+    demo.launch()
